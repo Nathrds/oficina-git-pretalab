c99e0c3aacc8ef8f849c220cc79bf24f4af1f4c0
Nathália Rodrigues <nathaliarsantos@live.com>
2023-01-21 17:15:27 -0300

    oficina-pretalab

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Olá Mundo!");
+
+const nomeUsuaria = prompt ("Me chamo Nathália, qual é o seu nome?");
+
+alert ("Bem vinda ao meu primeiro programa " + nomeUsuaria);
+alert (`Bem vinda ao meu primeiro programa ${nomeUsuaria}`);
